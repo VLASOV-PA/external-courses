71030a2d77bf07e394917f9cd18b0387624f0924
Commandos <pavelvlas_v@icloud.com>
2019-10-08 19:10:21 +0300

    changed the function name and variable

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -1,18 +1,18 @@
-function calcElements(arr){
-  let chet = 0 ,nechet = 0 ,zero = 0;
+function oddAndEvenElements(arr){
+  let even = 0 ,odd = 0 ,zero = 0;
   for (let i=0 ; i < arr.length; i++){
     if (typeof arr[i]=== "number"){
       if (arr[i]=== 0 ){
         zero += 1;
       } else if (arr[i] % 2 === 0 ){
-        chet += 1;
+        even += 1;
       } else if (arr[i] % 2 === 1 ){
-        nechet += 1;
+        odd += 1;
       } else continue
     }
   } 
-  console.log([chet,nechet,zero]);
-  return [chet,nechet,zero];
+  console.log([even,odd,zero]);
+  return [even,odd,zero];
 }
-module.exports = calcElements;
+module.exports = oddAndEvenElements;
 
