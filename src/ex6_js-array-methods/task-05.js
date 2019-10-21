10e046b3f768919d66836f3a44eeff6583e5a1a3
Commandos <pavelvlas_v@icloud.com>
2019-10-22 01:06:26 +0300

    Completed tasks 01-05 from ex6_js-array-methods

---
+++ src/ex6_js-array-methods/task-05.js
@@ -0,0 +1,8 @@
+function mapAnalog(array, callback, thisArg) {
+let mapResults = [];
+  for (let i = 0; i < array.length; i++) {
+    mapResults.push(callback.call(thisArg, array[i], i, array));
+  }
+  return mapResults;
+};
+module.exports = mapAnalog;
