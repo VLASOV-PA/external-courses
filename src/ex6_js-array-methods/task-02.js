c9d2ce9912593cb63eb8b41bdf9e1c9c14029915
Commandos <pavelvlas_v@icloud.com>
2019-10-22 12:50:16 +0300

    Completed tasks 01-05 from ex6_js-array-methods

--- src/ex6_js-array-methods/task-02.js
+++ src/ex6_js-array-methods/task-02.js
@@ -1,6 +1,6 @@
-function someAnalog(array, callback) {
+function someAnalog(array, callback,thisArg) {
   for (let i = 0; i < array.length; i++) {
-    if (callback.call(array[i], i, array)) {
+    if (callback.call(thisArg,array[i], i, array)) {
       return true;
     }
   }
