4bed77cb4ae8e17fb5eb7d039b2f8721de23f3f9
Commandos <pavelvlas_v@icloud.com>
2019-10-30 01:21:26 +0300

    new array pool

---
+++ src/ex6_js-array-methods/task-03.js
@@ -0,0 +1,9 @@
+function everyAnalog(arr, callback,thisArg) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!callback.call(thisArg,arr[i], i, arr)) {
+      return false;
+    }
+  }
+  return true;
+};
+module.exports = everyAnalog;
